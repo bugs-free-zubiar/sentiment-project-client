3cd8bdcd40f7dbd4fcb4718feeaa303b4b1b21b4
zubair <codingwithrock@gmail.com>
2023-02-06 13:23:08 +0600

    added client side for sentiment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+
+  // Keeping Sentiment data inside State
+  const [sentiment, setSentiment] = useState(null);
+
+
+  // Getting Sentiment Data
+  useEffect(() => {
+    fetch('http://localhost:5000/result')
+      .then(res => res.json())
+      .then(sentiment => setSentiment(sentiment))
+  }, [])
+
+
+  // stored Value
+  useEffect(() => {
+    console.log((sentiment?.score))
+  }, [sentiment?.score])
+  // console.log(sentiment)
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Calculation</h1>
+      {/* {console.log(Object.keys(sentiment?.calculation[0]))} */}
+      <h1>Negative</h1>
+      <h1>Positive</h1>
+      <h1>Score</h1>
+      <h1>Comparative</h1>
+      <h1>Tokens</h1>
+      <h1>Words</h1>
+
     </div>
   );
 }
